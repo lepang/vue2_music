78bf8e453b58c31bd335975f43b6a963b8b67bc8
chenwenpeng <1067357706@qq.com>
2018-02-04 21:21:54 +0800

    '4章节'

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'common/stylus/index.styl'
+import fastclick from 'fastclick'
+
+fastclick.attach(document.body)
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
